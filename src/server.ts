5779e1317872f27defaf6808446b16e262f3e735
kodakodu <kodakodu@gmail.com>
2022-09-06 16:34:25 +0200

    Mise en place express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello Yann');
+import express, { Request, Response } from 'express';
+
+//console.log('Hello Yann');
+const app = express();
+const port = 9000;
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log('Server start on port: ' + port);
+});
