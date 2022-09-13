c398301197effe6e2a98e3a11bcccf7b1326988b
kodakodu <kodakodu@gmail.com>
2022-09-13 17:12:02 +0200

    Validator

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import db from './config/database.config';
 import { v4 as uuidv4 } from 'uuid';
 import { TodoInstance } from './model';
+import TodoValidator from './validator';
+import { validationResult } from 'express-validator';
 
 db.sync().then(() => {
   console.log('connected to database');
@@ -16,20 +18,30 @@ app.use(express.json());
 /* app.get('/', (req: Request, res: Response) => {
   return res.send('Hello World');
 }); */
-app.post('/create', async (req: Request, res: Response) => {
-  const id = uuidv4();
-  try {
-    const record = await TodoInstance.create({ ...req.body, id });
-    return res.json({ record, msg: 'Successfully create Todo' });
-  } catch (e) {
-    return res.json({ msg: 'fail to create', status: 500, route: '/create' });
+app.post(
+  '/create',
+  TodoValidator.checkCreateTodo(),
+  (req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.json(error);
+    }
+    next();
+  },
+  async (req: Request, res: Response) => {
+    const id = uuidv4();
+    try {
+      const record = await TodoInstance.create({ ...req.body, id });
+      return res.json({ record, msg: 'Successfully create Todo' });
+    } catch (e) {
+      return res.json({ msg: 'fail to create', status: 500, route: '/create' });
+    }
   }
-});
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('OK Tutti !');
 });
-
 
 app.listen(port, () => {
   console.log('Server start on port: ' + port);
--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,3 +1,23 @@
+import { body } from 'express-validator';
+
 class TodoValidator {
-  checkCreateTodo() {}
+  checkCreateTodo() {
+    return [
+      body('id')
+        .optional()
+        .isUUID(4)
+        .withMessage('The value should be UUID v4'),
+      body('title')
+        .notEmpty()
+        .withMessage('The title value should not be empty'),
+      body('completed')
+        .optional()
+        .isBoolean()
+        .withMessage('The value should be boolean')
+        .isIn([0, false])
+        .withMessage('The value should be 0 or False'),
+    ];
+  }
 }
+
+export default new TodoValidator();
